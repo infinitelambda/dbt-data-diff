8f3483658cc9cb9c2125ef78e4a7aea1a9ba6618
Dat Nguyen <dat@infinitelambda>
2023-12-23 11:03:27 +0700

    feat: optimize refresh_configured_tables

--- macros/resources/refresh_configured_tables.sql
+++ macros/resources/refresh_configured_tables.sql
@@ -1,6 +1,7 @@
 {% macro refresh_configured_tables() %}
 
   {% set configured_tables = var("data_diff__configured_tables", []) %}
+  {% set configured_tables__fixed_naming = var("data_diff__configured_tables__fixed_naming", true) %}
   {% set configured_table_model -%} {{ ref("configured_tables") }} {%- endset %}
 
   {% set query -%}
@@ -12,17 +13,25 @@
     {% for item in configured_tables -%}
 
       select
-        '{{ item.get("src_db") }}' as src_db
-        ,'{{ item.get("src_schema") }}' as src_schema
-        ,'{{ item.get("src_table") }}' as src_table
-        ,'{{ item.get("trg_db") }}' as trg_db
-        ,'{{ item.get("trg_schema") }}' as trg_schema
-        ,'{{ item.get("trg_table") }}' as trg_table
-        ,'{{ item.get("pk") }}' as pk
-        ,{{ item.get("include_columns", []) | upper }} as include_columns
-        ,{{ item.get("exclude_columns", []) | upper }} as exclude_columns
-        ,True as is_enabled
-        ,'{{ item.get("pipe_name", "") }}' as pipe_name
+
+      {% if configured_tables__fixed_naming -%}
+          '{{ item.get("src_db", target.database) }}' as src_db
+          ,'{{ item.get("src_schema", target.schema) }}' as src_schema
+      {%- else -%}
+          '{{ generate_database_name(item.get("src_db")) }}' as src_db
+          ,'{{ generate_schema_name(item.get("src_schema")) }}' as src_schema
+      {%- endif -%}
+
+          ,'{{ item.get("src_table") }}' as src_table
+          ,'{{ item.get("trg_db", target.database) }}' as trg_db
+          ,'{{ item.get("trg_schema", target.schema) }}' as trg_schema
+          ,'{{ item.get("trg_table", item.get("src_table")) }}' as trg_table
+          ,'{{ item.get("pk") }}' as pk
+          ,{{ item.get("include_columns", []) | upper }} as include_columns
+          ,{{ item.get("exclude_columns", []) | upper }} as exclude_columns
+          ,True as is_enabled
+          ,'{{ item.get("pipe_name", "") }}' as pipe_name
+
       {% if not loop.last -%}
         union all
       {% endif %}
